3c8a861c3b82199b627027b20e59dc14479d0799
Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>
2024-11-04 16:41:45 +0100

    Fix wylaczania wtyczki

--- qgis_feed.py
+++ qgis_feed.py
@@ -55,7 +55,7 @@ class QgisFeed:
         """
         Function registers QGIS Feed
         """
-        print("New url: ",self.industry_url_short)
+        
         QgsMessageLog.logMessage('Registering feed')
         for key in self.s.allKeys():
             if self.envirosolutionsFeedPattern_old.match(key) or self.envirosolutionsFeedPattern_new.match(key):
@@ -73,7 +73,8 @@ class QgisFeed:
                 # sprawdz czy jest odpowiadajacy w qgis
                 if self.s.contains(re.sub(r'(\d+)', r'9999\1', key.replace(self.industry_url_short, 'httpsfeedqgisorg'))):
                     self.s.remove(key)
-                self.s.remove(key)
+                #self.s.remove(key)
+        self.s.sync()
 
     def checkIsFetchTime(self):
         """
